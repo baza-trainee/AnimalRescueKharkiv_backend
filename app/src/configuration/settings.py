f3057d4de00058b8b9f8691dbfdb66a46e1d5e06
Paul Kravchenko <22906567+PaulKravchenko@users.noreply.github.com>
2024-06-28 22:51:34 +1000

    feat: config settings updated with media cache and blob chunking settings

--- app/src/configuration/settings.py
+++ app/src/configuration/settings.py
@@ -21,9 +21,19 @@ class Settings(BaseSettings):
     cors_origins: str
     rate_limiter_times: int
     rate_limiter_seconds: int
+    blob_chunk_size: str = "10MB"  
+    media_cache_size: str = "400MB"      
+    media_cache_record_limit: str = "20MB"
+    blob_chunk_size_bytes: int = 10*1024*1024
+    media_cache_size_bytes: int = 400*1024*1024
+    media_cache_record_limit_bytes: int = 20*1024*1024
 
     model_config = ConfigDict(extra="ignore",
                               env_file=env_file if env_file.exists() else None,
                               env_file_encoding = "utf-8")
+
+    @property
+    def rate_limiter_description(self) -> str: 
+        f"No more than {self.rate_limiter_times} requests per {self.rate_limiter_seconds} seconds"
 
 settings = Settings()
