0f3524f85b8c7e868318ffed6c1ba96b07fe5881
Paul Kravchenko <22906567+PaulKravchenko@users.noreply.github.com>
2024-07-01 00:11:11 +1000

    fix: schemas code style fixes

--- app/src/media/schemas.py
+++ app/src/media/schemas.py
@@ -1,13 +1,24 @@
-import uuid
 import enum
-from typing import Dict, Hashable, List, Optional, Annotated, TypeVar
-from datetime import datetime
-from pydantic import BaseModel, ConfigDict, Field, EmailStr, PastDate, PlainSerializer, Strict, conset, UUID4, computed_field
-from datetime import date
+import uuid
+from datetime import date, datetime
+from typing import Annotated, Dict, Hashable, List, Optional, TypeVar
+
+from pydantic import (
+    UUID4,
+    BaseModel,
+    ConfigDict,
+    EmailStr,
+    Field,
+    PastDate,
+    PlainSerializer,
+    Strict,
+    computed_field,
+    conset,
+)
 
 UUIDString = Annotated[UUID4, PlainSerializer(lambda x: str(x), return_type=str)]
 
-class MediaAssetInfo(BaseModel):       
+class MediaAssetInfo(BaseModel):
     blob_id: UUIDString
     extension: str
     content_type: str
@@ -16,7 +27,7 @@ class MediaAssetInfo(BaseModel):
 
     model_config = ConfigDict(from_attributes=True)
 
-class MediaAssetResponse(BaseModel):           
+class MediaAssetResponse(BaseModel):
     id: UUIDString
     extension: str
     content_type: str
@@ -25,7 +36,8 @@ class MediaAssetResponse(BaseModel):
 
     model_config = ConfigDict(from_attributes=True)
 
-    @computed_field
     @property
+    @computed_field
     def uri(self) -> str:
+        """Returns preformatted media URI"""
         return f"/media/{self.id}"
