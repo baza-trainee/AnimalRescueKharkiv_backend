9ff9161a6ac7db5fe71f465128cff2817762f2b2
Paul Kravchenko <22906567+PaulKravchenko@users.noreply.github.com>
2024-07-01 13:49:20 +1000

    feat: singleton metaclass implemented

---
+++ app/src/singleton.py
@@ -0,0 +1,16 @@
+from abc import ABCMeta
+from typing import Any, ClassVar
+
+
+class SingletonMeta(ABCMeta):
+
+    _instances: ClassVar = {}
+
+    def __call__(cls, *args, **kwargs) -> (Any | None): #noqa:ANN401
+        """Creates a singleton instance of a type"""
+        if cls.__module__.startswith("src."):
+            if cls not in cls._instances:
+                instance = super().__call__(*args, **kwargs)
+                cls._instances[cls] = instance
+            return cls._instances[cls]
+        return None
