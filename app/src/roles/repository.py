5a2e9c86095071c52ae4e6346cdf78283dd0b11f
Paul Kravchenko <22906567+PaulKravchenko@users.noreply.github.com>
2024-07-28 00:10:42 +1000

    fix: roles select queries updated with unique

--- app/src/roles/repository.py
+++ app/src/roles/repository.py
@@ -15,7 +15,8 @@ logger = logging.getLogger(uvicorn.logging.__name__)
 class RolesRepository (metaclass=SingletonMeta):
     async def create_role(self, model: RoleBase, db: AsyncSession) -> Role:
         """Creates a role definition. Returns the created role definition"""
-        role = Role(name=model.name.lower(), domain=model.domain.lower())
+        permissions: list[Permission] = []
+        role = Role(name=model.name.lower(), domain=model.domain.lower(), permissions = permissions)
         db.add(role)
         await db.commit()
         await db.refresh(role)
@@ -26,7 +27,7 @@ class RolesRepository (metaclass=SingletonMeta):
         statement = select(Role)
         statement = statement.filter_by(name=model.name.lower(), domain=model.domain.lower())
         result = await db.execute(statement)
-        return result.scalar_one_or_none()
+        return result.unique().scalar_one_or_none()
 
     async def read_roles(self, name:str, domain:str, db: AsyncSession) -> list[Role]:
         """Reads all roles with optional filtering. Returns the retrieved collection of roles"""
@@ -36,7 +37,7 @@ class RolesRepository (metaclass=SingletonMeta):
         if domain:
             statement = statement.filter_by(domain=domain.lower())
         result = await db.execute(statement)
-        roles = result.scalars().all()
+        roles = result.unique().scalars().all()
         return list(roles)
 
     async def delete_role(self, role: Role, db: AsyncSession) -> Role | None:
