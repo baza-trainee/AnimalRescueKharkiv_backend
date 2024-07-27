0898eae75589e2257b2267001873987cac25b5af
Paul Kravchenko <22906567+PaulKravchenko@users.noreply.github.com>
2024-07-28 00:08:58 +1000

    feat: premission model updated with roles relation

--- app/src/permissions/models.py
+++ app/src/permissions/models.py
@@ -1,11 +1,12 @@
 import uuid
+from typing import TYPE_CHECKING
 
 from sqlalchemy import UUID, String, UniqueConstraint
-from sqlalchemy.orm import Mapped, declarative_base, mapped_column
-from sqlalchemy.orm.decl_api import DeclarativeMeta
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from src.configuration.db import Base
 
-#Base: DeclarativeMeta = declarative_base()
+if TYPE_CHECKING:
+    from src.roles.models import Role
 
 
 class Permission(Base):
@@ -16,3 +17,6 @@ class Permission(Base):
     id: Mapped[uuid.UUID] = mapped_column(UUID, primary_key=True, default=uuid.uuid1)
     entity: Mapped[str] = mapped_column(String(50), nullable=False, index=True)
     operation: Mapped[str] = mapped_column(String(50), nullable=False, index=True)
+    roles: Mapped[list["Role"]] = relationship(secondary="roles_permissions",
+                                                           back_populates="permissions",
+                                                           lazy="joined")
