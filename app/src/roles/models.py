b64b6059b236be2ca83150ecbab894a8f67c6c17
Yuliia Chorna <140984907+YuliiaChorna1@users.noreply.github.com>
2024-07-27 23:08:52 +1000

    feat: permissions to roles relation added

--- app/src/roles/models.py
+++ app/src/roles/models.py
@@ -1,10 +1,12 @@
 import uuid
 
-from sqlalchemy import UUID, String, UniqueConstraint
-from sqlalchemy.orm import Mapped, declarative_base, mapped_column
+from sqlalchemy import UUID, ForeignKey, String, UniqueConstraint
+from sqlalchemy.orm import Mapped, declarative_base, mapped_column, relationship
 from sqlalchemy.orm.decl_api import DeclarativeMeta
+from src.configuration.db import Base
+from src.permissions.models import Permission
 
-Base: DeclarativeMeta = declarative_base()
+#Base: DeclarativeMeta = declarative_base()
 
 
 class Role(Base):
@@ -15,3 +17,13 @@ class Role(Base):
     id: Mapped[uuid.UUID] = mapped_column(UUID, primary_key=True, default=uuid.uuid1)
     name: Mapped[str] = mapped_column(String(50), nullable=False, index=True)
     domain: Mapped[str] = mapped_column(String(20), nullable=False, index=True)
+    permissions: Mapped[list["Permission"]] = relationship(secondary="roles_permissions",
+                                                           back_populates="roles",
+                                                           lazy="joined")
+
+
+class RolePermission(Base):
+    __tablename__ = "roles_permissions"
+    id: Mapped[uuid.UUID] = mapped_column(UUID, primary_key=True, default=uuid.uuid1)
+    role_id: Mapped[uuid.UUID] = mapped_column(ForeignKey(Role.id, ondelete="CASCADE"), nullable=False)
+    permission_id: Mapped[int] = mapped_column(ForeignKey(Permission.id), nullable=False)
