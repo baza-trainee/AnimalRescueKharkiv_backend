29dbef5118c32730f99c1e786ae251e87df6853d
DaTrEvTeR <rozhkovm176@gmail.com>
2024-07-29 17:06:21 +0300

    feat: created user model
    - create user model
    - add relationship to users for role model
    - migration file made
    - fix env.py
    - add sqlalchemy_utils to dependencies

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -7,6 +7,11 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 from src import *  # noqa: F403
 from src.configuration.db import SQLALCHEMY_DATABASE_URL, Base
+from src.media.models import *
+from src.roles.models import *
+from src.permissions.models import *
+from src.users.models import *
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -21,7 +26,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base
+target_metadata = Base.metadata
 
 config.set_main_option("sqlalchemy.url", SQLALCHEMY_DATABASE_URL)
 # other values from the config, defined by the needs of env.py,
