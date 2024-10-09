5a20ca3bd62e54c81d5838cc5199f366fbd6c650
Yuliia Chorna <140984907+YuliiaChorna1@users.noreply.github.com>
2024-10-09 23:56:49 +1100

    feat: user model updated with extra attributes

--- app/src/users/models.py
+++ app/src/users/models.py
@@ -4,19 +4,21 @@ from sqlalchemy import UUID, ForeignKey, String, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy_utils import EmailType, PasswordType
 from src.configuration.db import Base
+from src.media.models import MediaAsset
 from src.roles.models import Role
 
 
 class User(Base):
     __tablename__ = "users"
-    __table_args__ = (
-        UniqueConstraint("username", "domain", name="username_domain_unique"),
-        UniqueConstraint("email", "domain", name="email_domain_unique"),
-    )
+    __table_args__ = (UniqueConstraint("email", "domain", name="email_domain_unique"),)
     id: Mapped[UUID] = mapped_column(UUID, primary_key=True, default=uuid4)
-    username: Mapped[str] = mapped_column(String(50), nullable=False, index=True)
-    email: Mapped[EmailType] = mapped_column(EmailType(), nullable=False)
+    email: Mapped[EmailType] = mapped_column(EmailType(), index=True, nullable=False)
     domain: Mapped[str] = mapped_column(String(20), nullable=False, index=True)
     password: Mapped[PasswordType] = mapped_column(PasswordType(schemes=["bcrypt"]), nullable=False)
+    first_name: Mapped[str] = mapped_column(String(50), index=False, nullable=True)
+    last_name: Mapped[str] = mapped_column(String(50), index=False, nullable=True)
+    phone: Mapped[str] = mapped_column(String(30), index=False, unique=True, nullable=True)
+    photo_id: Mapped[MediaAsset] = mapped_column(ForeignKey(MediaAsset.id, ondelete="SET NULL"), nullable=True)
+    photo: Mapped[MediaAsset] = relationship(MediaAsset, lazy="joined")
     role_id: Mapped[Role] = mapped_column(ForeignKey(Role.id, ondelete="SET NULL"), nullable=True)
     role: Mapped[Role] = relationship(Role, back_populates="users", lazy="joined")
