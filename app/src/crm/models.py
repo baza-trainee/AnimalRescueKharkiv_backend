30d9e42f861f17257027747804b751fdcc4773ac
Yuliia Chorna <140984907+YuliiaChorna1@users.noreply.github.com>
2025-01-31 00:35:15 +1100

    feat: crm module with Animal model added

---
+++ app/src/crm/models.py
@@ -0,0 +1,120 @@
+from typing import TYPE_CHECKING, List
+from uuid import uuid4
+
+from sqlalchemy import UUID, Boolean, Date, DateTime, Float, ForeignKey, Integer, String, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from src.configuration.db import Base
+from src.media.models import MediaAsset
+
+if TYPE_CHECKING:
+    from src.users.models import User
+
+class Animal(Base):
+    __tablename__ = "crm_animals"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(50), index=True, nullable=False)
+
+    origin__arrival_date: Mapped[Date] = mapped_column(Date, index=True, nullable=False)
+    origin__city: Mapped[str] = mapped_column(String(100), index=True, nullable=False)
+    origin__address: Mapped[str] = mapped_column(String(100), index=False, nullable=True)
+
+    general__animal_type: Mapped[str] = mapped_column(String(50), index=True, nullable=False)
+    general__gender: Mapped[str] = mapped_column(String(10), index=True, nullable=False)
+    general__weight: Mapped[float] = mapped_column(Float, index=False, nullable=True)
+    general__age: Mapped[float] = mapped_column(Float, index=False, nullable=True)
+    general__specials: Mapped[str] = mapped_column(String(200), index=False, nullable=True)
+
+    owner__info: Mapped[str] = mapped_column(String(500), index=False, nullable=True)
+
+    comment__text: Mapped[str] = mapped_column(String(1000), index=False, nullable=True)
+
+    adoption__country: Mapped[str] = mapped_column(String(50), index=False, nullable=True)
+    adoption__city: Mapped[str] = mapped_column(String(50), index=False, nullable=True)
+    adoption__date: Mapped[Date] = mapped_column(Date, index=False, nullable=False)
+    adoption__comment: Mapped[str] = mapped_column(String(500), index=False, nullable=True)
+
+    death__dead: Mapped[bool] = mapped_column(Boolean, default=False, index=True)
+    death__date: Mapped[Date] = mapped_column(Date, index=True, nullable=True)
+    death__comment: Mapped[str] = mapped_column(String(500), index=False, nullable=True)
+
+    sterilization__done: Mapped[bool] = mapped_column(Boolean, default=False, index=True)
+    sterilization__date: Mapped[Date] = mapped_column(Date, index=True, nullable=True)
+    sterilization__comment: Mapped[str] = mapped_column(String(500), index=False, nullable=True)
+
+    microchipping__done: Mapped[bool] = mapped_column(Boolean, default=False, index=True)
+    microchipping__date: Mapped[Date] = mapped_column(Date, index=True, nullable=True)
+    microchipping__comment: Mapped[str] = mapped_column(String(500), index=False, nullable=True)
+
+    updated_at: Mapped[DateTime] = mapped_column(DateTime, default=func.now(), onupdate=func.now(), index=True)
+    updated_by_id: Mapped[UUID] = mapped_column(UUID, ForeignKey("users.id"), nullable=False)
+    updated_by: Mapped["User"] = relationship("User", backref="crm_animals", lazy="joined")
+
+    created_at: Mapped[DateTime] = mapped_column(DateTime, default=func.now(), index=True)
+    created_by_id: Mapped[UUID] = mapped_column(UUID, ForeignKey("users.id"), nullable=False)
+    created_by: Mapped["User"] = relationship("User", backref="crm_animals", lazy="joined")
+
+    media: Mapped[List[MediaAsset]] = relationship(secondary="crm_animal_media",
+                                                   back_populates="crm_animals",
+                                                   lazy="joined")
+    locations: Mapped[List["AnimalLocation"]] = relationship("AnimalLocation",
+                                                             back_populates="crm_animals",
+                                                             lazy="joined")
+    vaccinations: Mapped[List["Vaccination"]] = relationship("Vaccination",
+                                                             back_populates="crm_animals",
+                                                             lazy="joined")
+    diagnoses: Mapped[List["Diagnosis"]] = relationship("Diagnosis",
+                                                        back_populates="crm_animals",
+                                                        lazy="joined")
+    procedures: Mapped[List["Procedure"]] = relationship("Procedure",
+                                                         back_populates="crm_animals",
+                                                         lazy="joined")
+
+
+class Location(Base):
+    __tablename__ = "crm_locations"
+    id: Mapped[UUID] = mapped_column(UUID, primary_key=True, default=uuid4)
+    name: Mapped[str] = mapped_column(String(100), index=False, nullable=False)
+
+
+class AnimalLocation(Base):
+    __tablename__ = "crm_animal_locations"
+    id: Mapped[UUID] = mapped_column(UUID, primary_key=True, default=uuid4)
+    animal_id: Mapped[str] = mapped_column(ForeignKey(Animal.id, ondelete="CASCADE"), nullable=False)
+    location_id: Mapped[UUID] = mapped_column(ForeignKey(Location.id), nullable=False)
+    date_from: Mapped[Date] = mapped_column(Date, index=False, nullable=False)
+    date_to: Mapped[Date] = mapped_column(Date, index=False, nullable=True)
+
+
+class AnimalMedia(Base):
+    __tablename__ = "crm_animal_media"
+    id: Mapped[UUID] = mapped_column(UUID, primary_key=True, default=uuid4)
+    animal_id: Mapped[str] = mapped_column(ForeignKey(Animal.id, ondelete="CASCADE"), nullable=False)
+    media_id: Mapped[MediaAsset] = mapped_column(ForeignKey(MediaAsset.id, ondelete="CASCADE"), nullable=True)
+
+
+class Vaccination(Base):
+    __tablename__ = "crm_vaccinations"
+    id: Mapped[UUID] = mapped_column(UUID, primary_key=True, default=uuid4)
+    animal_id: Mapped[str] = mapped_column(ForeignKey(Animal.id, ondelete="CASCADE"), nullable=False)
+    is_vaccinated: Mapped[bool] = mapped_column(Boolean, default=False, index=True)
+    vaccine_type: Mapped[str] = mapped_column(String(100), index=False, nullable=True)
+    date: Mapped[Date] = mapped_column(Date, index=True, nullable=True)
+    comment: Mapped[str] = mapped_column(String(500), index=False, nullable=True)
+
+
+class Diagnosis(Base):
+    __tablename__ = "crm_diagnoses"
+    id: Mapped[UUID] = mapped_column(UUID, primary_key=True, default=uuid4)
+    animal_id: Mapped[str] = mapped_column(ForeignKey(Animal.id, ondelete="CASCADE"), nullable=False)
+    name: Mapped[str] = mapped_column(String(100), index=False, nullable=True)
+    date: Mapped[Date] = mapped_column(Date, index=False, nullable=True)
+    comment: Mapped[str] = mapped_column(String(500), index=False, nullable=True)
+
+
+class Procedure(Base):
+    __tablename__ = "crm_procedures"
+    id: Mapped[UUID] = mapped_column(UUID, primary_key=True, default=uuid4)
+    animal_id: Mapped[str] = mapped_column(ForeignKey(Animal.id, ondelete="CASCADE"), nullable=False)
+    name: Mapped[str] = mapped_column(String(100), index=False, nullable=True)
+    date: Mapped[Date] = mapped_column(Date, index=False, nullable=True)
+    comment: Mapped[str] = mapped_column(String(500), index=False, nullable=True)
