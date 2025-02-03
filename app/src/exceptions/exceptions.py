41bfaa69a477db855c1987a5810222bb06bc9268
Yuliia Chorna <140984907+YuliiaChorna1@users.noreply.github.com>
2025-02-03 16:59:19 +1100

    feat: exception message simplified

--- app/src/exceptions/exceptions.py
+++ app/src/exceptions/exceptions.py
@@ -32,7 +32,7 @@ class ReturnMessages:
 
     access_denied: str = "Access denied. Required permissions: %s"
 
-    crm_animal_type_not_found: str = "Animal type '%s' not found"
+    crm_animal_type_not_found: str = "Animal type not found"
     crm_animal_not_found: str = "Animal not found"
     crm_location_not_found: str = "Location not found"
     crm_illegal_sort: str = "Sort expression should be in format {field}|{direction}"
