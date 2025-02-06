2414e0720d1da5397a484ceb9d88fcc8f1e4ea87
Paul Kravchenko <22906567+PaulKravchenko@users.noreply.github.com>
2025-02-06 16:02:50 +1100

    feat: implemented update strategies for animal object

---
+++ app/src/crm/strategies.py
@@ -0,0 +1,258 @@
+import inspect
+import logging
+import re
+import uuid
+from abc import ABC, abstractmethod
+from datetime import date
+from typing import Any, Awaitable, Callable, Dict, List, Tuple, TypeVar
+from uuid import UUID
+
+import uvicorn
+from sqlalchemy import Select, and_, asc, desc, func, or_
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.sql import ColumnElement, ColumnExpressionArgument
+from sqlalchemy.sql.elements import UnaryExpression
+from src.configuration.settings import settings
+from src.crm.models import Animal, AnimalLocation, AnimalType, Diagnosis, Gender, Location, Procedure, Vaccination
+from src.crm.repository import animals_repository
+from src.crm.schemas import (
+    SORTING_VALIDATION_REGEX,
+    AnimalCreate,
+    AnimalLocationBase,
+    AnimalState,
+    AnimalTypeBase,
+    BaseModel,
+    DiagnosisBase,
+    IntReferenceBase,
+    LocationBase,
+    ProcedureBase,
+    UUIDReferenceBase,
+    VaccinationBase,
+)
+from src.exceptions.exceptions import RETURN_MSG
+from src.media.models import MediaAsset
+from src.singleton import SingletonMeta
+from src.users.models import User
+
+
+class UpdateStrategy(ABC):
+    @abstractmethod
+    async def update(self,
+                     model: Animal,
+                     update_model: BaseModel,
+                     user: User,
+                     db: AsyncSession,
+                    ) -> Animal:
+        """Updates animal based on passed model. Returns the updated animal"""
+
+class UpdateAttrsStrategy(UpdateStrategy):
+    def __init__(self,
+                 not_fount_msg: str | None = None,
+                 read_def_func: Callable[[int, AsyncSession], Awaitable[DeclarativeBase]] | None = None,
+                ) -> None:
+        """Initializes strategy"""
+        self.__not_fount_msg: str | None = not_fount_msg
+        self.__read_def_func: Callable[[int, AsyncSession], Awaitable[DeclarativeBase]] | None = read_def_func
+
+    async def update(self,
+                     model: Animal,
+                     update_model: BaseModel,
+                     user: User,
+                     db: AsyncSession,
+                    ) -> Animal:
+        """Updates animal based on passed model. Returns the updated animal"""
+        if not update_model:
+            return model
+        definition: DeclarativeBase = None
+        for field_name in update_model.model_fields:
+                field = getattr(update_model, field_name, None)
+                if isinstance(field, IntReferenceBase) and self.__read_def_func:
+                    definition = await self.__read_def_func(field.id, db)
+                    if not definition:
+                        raise ValueError(self.__not_fount_msg)
+
+        return await animals_repository.update_animal_fields(
+            update_model=update_model,
+            model=model,
+            user=user,
+            db=db)
+
+class UpdateRefsStrategy(UpdateStrategy):
+    def __init__(self,
+                 ref_name: str,
+                 add_func: Callable[...,Awaitable[DeclarativeBase]] | None = None,
+                 read_def_func: Callable[[int, AsyncSession], Awaitable[DeclarativeBase]] | None = None,
+                 update_func: Callable[..., Awaitable[DeclarativeBase]] | None = None,
+                ) -> None:
+        """Initializes strategy"""
+        self.__ref_name: str = ref_name
+        self.__add_func: Callable[...,Awaitable[DeclarativeBase]] | None = add_func
+        self.__read_def_func: Callable[[int, AsyncSession], Awaitable[DeclarativeBase]] | None = read_def_func
+        self.__update_func: Callable[..., Awaitable[DeclarativeBase]] | None = update_func
+
+    async def update(self,
+                     model: Animal,
+                     update_model: BaseModel,
+                     user: User,
+                     db: AsyncSession,
+                    ) -> Animal:
+        """Updates animal based on passed model. Returns the updated animal"""
+        if not update_model:
+            return model
+        refs: List[BaseModel] = update_model if isinstance(update_model, list) else []
+        if not refs:
+            for field_name in update_model.model_fields:
+                    field = getattr(update_model, field_name, None)
+                    if isinstance(field, list):
+                        refs = field
+        for ref in refs:
+            model = await self.__update_single(model=model,
+                                 update_model=ref,
+                                 user=user,
+                                 db=db)
+        return model
+
+    def __get_ref_by_id(self, ref_id:UUID, model: Animal) -> DeclarativeBase | None:
+        references: List[DeclarativeBase] | None = getattr(model, self.__ref_name, None)
+        if references:
+            return next((ref for ref in references if ref.id == ref_id), None)
+        return None
+
+    async def __update_single(self,
+                     model: Animal,
+                     update_model: BaseModel,
+                     user: User,
+                     db: AsyncSession,
+                    ) -> Animal:
+        ref_obj: DeclarativeBase = None
+        definition: DeclarativeBase = None
+        for field_name in update_model.model_fields:
+                field = getattr(update_model, field_name, None)
+                if isinstance(field, UUID):
+                    ref_obj = self.__get_ref_by_id(ref_id=field, model=model)
+                if isinstance(field, IntReferenceBase) and self.__read_def_func:
+                    definition = await self.__read_def_func(field.id, db)
+        if ref_obj:
+            model = await self.__update_reference(model=model,
+                                                  ref_obj=ref_obj,
+                                                  definition=definition,
+                                                  update_model=update_model,
+                                                  user=user,
+                                                  db=db)
+        else:
+            model = await self.__add_reference(model=model,
+                                               update_model=update_model,
+                                               definition=definition,
+                                               user=user,
+                                               db=db)
+        return model
+
+    async def __add_reference(self,
+                              model: Animal,
+                              update_model: BaseModel,
+                              definition: DeclarativeBase,
+                              user: User,
+                              db: AsyncSession,
+                             ) -> Animal:
+        if not self.__add_func:
+            return model
+
+        if definition:
+            if "model" in inspect.getfullargspec(self.__add_func).args:
+                model = await self.__add_func(
+                            definition=definition,
+                            model=update_model,
+                            animal=model,
+                            user=user,
+                            db=db)
+            else:
+                model = await self.__add_func(
+                            definition=definition,
+                            animal=model,
+                            user=user,
+                            db=db)
+        else:
+            model = await self.__add_func(
+                        model=update_model,
+                        animal=model,
+                        user=user,
+                        db=db)
+        return model
+
+    async def __update_reference(self,
+                              model: Animal,
+                              ref_obj: DeclarativeBase,
+                              definition: DeclarativeBase,
+                              update_model: BaseModel,
+                              user: User,
+                              db: AsyncSession,
+                            ) -> Animal:
+        if not self.__update_func:
+            return model
+
+        if definition:
+            model = await self.__update_func(update_model=update_model,
+                                                definition=definition,
+                                                model=ref_obj,
+                                                user=user,
+                                                db=db)
+        else:
+            model = await self.__update_func(update_model=update_model,
+                                                model=ref_obj,
+                                                user=user,
+                                                db=db)
+        return model
+
+
+class UpdateHanlder(metaclass=SingletonMeta):
+    def __init__(self) -> None:
+        """Initializes Update Handler with startegies"""
+        self.__strategies: Dict[str, UpdateStrategy] = self.__init_strategies()
+
+    def __init_strategies(self) -> Dict[str, UpdateStrategy]:
+        return {
+            "name": UpdateAttrsStrategy(),
+            "origin": UpdateAttrsStrategy(),
+            "general": UpdateAttrsStrategy(not_fount_msg=RETURN_MSG.crm_animal_type_not_found,
+                                           read_def_func=animals_repository.read_animal_type),
+            "owner": UpdateAttrsStrategy(),
+            "comment": UpdateAttrsStrategy(),
+            "adoption": UpdateAttrsStrategy(),
+            "death": UpdateAttrsStrategy(),
+            "sterilization": UpdateAttrsStrategy(),
+            "microchipping": UpdateAttrsStrategy(),
+            "media": UpdateRefsStrategy(ref_name="media",
+                                        add_func=animals_repository.add_media_to_animal),
+            "locations": UpdateRefsStrategy(ref_name="locations",
+                                            add_func=animals_repository.add_animal_location,
+                                            read_def_func=animals_repository.read_location,
+                                            update_func=animals_repository.update_animal_location),
+            "vaccinations": UpdateRefsStrategy(ref_name="vaccinations",
+                                            add_func=animals_repository.add_vaccination_to_animal,
+                                            update_func=animals_repository.update_vaccination),
+            "diagnoses": UpdateRefsStrategy(ref_name="diagnoses",
+                                            add_func=animals_repository.add_diagnosis_to_animal,
+                                            update_func=animals_repository.update_diagnosis),
+            "procedures": UpdateRefsStrategy(ref_name="procedures",
+                                            add_func=animals_repository.add_procedure_to_animal,
+                                            update_func=animals_repository.update_procedure),
+        }
+
+    async def handle_update(self,
+                      section_name: str,
+                      model: Animal,
+                      update_model: BaseModel,
+                      user: User,
+                      db: AsyncSession,
+                    ) -> Animal:
+        """Hnadles animal updates via strategies based on section name. Returns the updated animal"""
+        strategy: UpdateStrategy = self.__strategies[section_name]
+
+        return await strategy.update(model=model,
+                                      update_model=update_model,
+                                      user=user,
+                                      db=db)
+
+update_handler: UpdateHanlder = UpdateHanlder()
