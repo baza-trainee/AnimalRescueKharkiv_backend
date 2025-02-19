d900737a4c62789749afe49b5211c564b437f366
Yuliia Chorna <140984907+YuliiaChorna1@users.noreply.github.com>
2025-02-19 21:12:44 +1100

    chore: refactor, code clean up

--- app/src/authorization/service.py
+++ app/src/authorization/service.py
@@ -16,18 +16,6 @@ from src.users.models import User
 logger = logging.getLogger(uvicorn.logging.__name__)
 
 
-# Authorization_service.authorize
-# Resolve context_user and scopes from access_token
-# Compare permissions on the route against scopes in the access_token
-# If at least single required permission(from the route) is not found on the user throw HTTP 403 exception
-
-# Authorization_service.pre_authorize
-# Resolve context_user and permissions from access_token
-# Loop through all the model attributes
-# For each attribute check if context_user has write access to it
-# If user hass write acces add the attribtue to the list editable_attibutes
-
-
 class Authorization(metaclass=SingletonMeta):
     async def authorize_user(
             self, scopes: SecurityScopes,
@@ -63,20 +51,6 @@ class Authorization(metaclass=SingletonMeta):
 
     def __is_system_admin(self, user: User) -> bool:
         return (user.role.name == settings.super_user_role) and (user.role.domain == settings.super_user_domain)
-
-    def authorize_model_attributes(self, model: BaseModel, user: User) -> BaseModel:
-        """Authorizes user access to response model attibutes. Returns the authorized response model"""
-        editable_attributes = []
-        logger.info(user.email)
-        for field_name, field_info in model.model_fields.items():
-            if field_name == "editable_attributes":
-                continue
-            attr_name = field_name
-            if isinstance(field_info, FieldInfo) and field_info.alias:
-                attr_name = field_info.alias
-            #TODO: implement the permissions authorization
-            editable_attributes.append(attr_name)
-        return model.model_copy(update={"editable_attributes": editable_attributes})
 
 
 authorization_service: Authorization = Authorization()
